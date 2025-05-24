8ca1a19989c61580c314bd48481725f4a57e5319
Pall Pandiyan.S <pallpandiyan@gmail.com>
2025-05-24 22:52:00 +0530

    updated

---
+++ grocery_list/src/pages/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Button from "../components/Button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 onClick={() => setCount(count + 1)}>Hello App: {count}</h1>
+      <Button color="secondary" onClick={(e) => console.log(e)}>
+        Click Me!
+      </Button>
+    </>
+  );
+}
+
+export default App;
