b1fb559986cc751e1bddf5389e329791174bbf48
Pall Pandiyan.S <pallpandiyan@gmail.com>
2025-06-08 20:48:01 +0530

    layout start

--- game_store/src/App.tsx
+++ game_store/src/App.tsx
@@ -1,7 +1,17 @@
+import { HStack, VStack, Box } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <h1>Testing</h1>
+      <HStack>
+        <Box border="1px red solid">Sidebar</Box>
+        <Box border="1px blue solid">
+          <VStack>
+            <Box border="1px yellow solid">Topbar</Box>
+            <Box border="1px green solid">Main</Box>
+          </VStack>
+        </Box>
+      </HStack>
     </>
   );
 }
